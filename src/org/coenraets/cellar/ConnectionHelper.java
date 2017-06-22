0021445f1671c5a1516c9e3312583795e89bd40e
marcospr <marcospinheirorocha@hotmail.com>
2017-06-22 14:51:47 -0300

    1a versao

---
+++ src/org/coenraets/cellar/ConnectionHelper.java
@@ -0,0 +1,51 @@
+package org.coenraets.cellar;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectionHelper {
+	private String url;
+	private String user;
+	private String password;
+	private static ConnectionHelper instance;
+
+	private ConnectionHelper() {
+		String driver = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			url = "jdbc:mysql://localhost/directory?user=root";
+			ResourceBundle bundle = ResourceBundle.getBundle("cellar");
+			driver = bundle.getString("jdbc.driver");
+			Class.forName(driver);
+			url = bundle.getString("jdbc.url");
+			user = bundle.getString("jdbc.user");
+			password = bundle.getString("jdbc.password");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		if (instance == null) {
+			instance = new ConnectionHelper();
+		}
+		try {
+			return DriverManager.getConnection(instance.url, instance.user, instance.password);
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	public static void close(Connection connection) {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
